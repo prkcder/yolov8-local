e88a70b12c6d934d2bfde3556543b3386971cae1
john <jhnprk12@gmail.com>
2025-06-23 01:59:33 -0700

    roboflow

--- inference.py
+++ inference.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
 model = YOLO("runs/detect/train4/weights/best.pt")
-results = model("dataset/valid/images/zidane.jpg", conf=0.1)
+results = model("test/images/car.jpg")
 # results[0].show(filename="output.jpg")  # Display
 results[0].save(filename="output.jpg")  # Save result
